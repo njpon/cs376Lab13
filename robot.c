af1049cdcfe7a9cb9451cb6a244c9844101070c1
Alex Mak <alexanderajm@gmail.com>
2020-11-24 10:23:12 -0800

    Gave the robot a facelift.  Added myself to author list.

--- robot.c
+++ robot.c
@@ -4,7 +4,7 @@
  * A file that contains the supporting functions for main() in the
  * ~/robots example source for Software Engineering.
  *
- * @author Tanya L. Crenshaw, Nathan Schmedake
+ * @author Tanya L. Crenshaw, Nathan Schmedake, Alex Mak
  * @since August 2013
  *
  */
@@ -16,7 +16,7 @@
  *
  * This function prints the message, "I really love robots!"
  *
- * @returns none 
+ * @returns none
  */
 void robotPrintMessage(void)
 {
@@ -54,16 +54,16 @@ void dalekPrintAscii(void)
   blank = blank +30;
   printf("\n\n"
 	 "I am Dalek, from Dr. Who\n"
-	 "%s      <----->      \n" 
-         "%s     <  (0)  >     \n"        
-         "%s     |       |     \n"
+	 "%s      <----->      \n"
+         "%s     <  (0)  >     \n"
+         "%s     ---------     \n"
          "%s    < ------- >    \n"
          "%s    o         o    \n"
          "%s    o  0  ()  o    \n"
 	 "%s   o           o   \n"
    	 "%s o o o o o o o o o \n"
    	 "%s o o o o o o o o o \n"
-   	 "%s o o o o o o o o o \n", blank, blank, blank, blank, blank, blank, 
+   	 "%s o o o o o o o o o \n", blank, blank, blank, blank, blank, blank,
 	 blank, blank, blank, blank);
   blank = blank - 10;
 }
